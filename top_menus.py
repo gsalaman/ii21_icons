8547a9e775892f03981ff6b2e4ed2f5a789ea924
gsalaman <g.salaman@comcast.net>
2021-03-19 14:52:31 +0000

    fix top menu bugs

--- top_menus.py
+++ top_menus.py
@@ -1,5 +1,5 @@
 import time 
-import datetime
+from datetime import datetime
 import serial
 #import get_buttons
 
@@ -36,8 +36,8 @@ matrix = RGBMatrix(options = options)
 seconds_per_screen = 1
 
 blank_screen = Image.new("RGB", (total_columns,total_rows))
-home1 = Image.open("top/homescreen.gif01.gif").convert("RGB")
-home2 = Image.open("top/homescreen.gif02.gif").convert("RGB")
+home1 = Image.open("top/Homescreen.gif01.gif").convert("RGB")
+home2 = Image.open("top/Homescreen.gif02.gif").convert("RGB")
 home_images = (home1, home2)
 num_home_images = len(home_images)
 home_image_index = 0
@@ -49,18 +49,18 @@ try:
   last_update_time = datetime.now()
 
   while True:
-    ```
+    ''' 
     my_button = get_buttons.read()
     if my_button != None:
       # This is where we'd go to the next screen....but right now, we'll exit.
       exit(0)
-    ```
+    ''' 
 
     current_time = datetime.now()
     deltaT = current_time - last_update_time
  
     # check to see if it's time to switch the animated gif
-    if ((deltaT.total_seconds() > seconds_per_screen):
+    if deltaT.total_seconds() > seconds_per_screen:
       home_image_index = (home_image_index + 1) % num_home_images
       matrix.SetImage(home_images[home_image_index],0,0)
       last_update_time = current_time
